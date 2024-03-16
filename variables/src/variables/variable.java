b021fb69d45c513aecfd13f6aec3526ec292e2c8
Nisha Ramprasath <115043583+nisharampra@users.noreply.github.com>
2024-03-16 23:30:32 +0800

    Add files via upload

---
+++ variables/src/variables/variable.java
@@ -0,0 +1,23 @@
+package variables;
+
+public class variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int x=123;//initilitaion
+       float y=2.14f;
+       double z=4;
+       boolean j=true;
+       char symbol='*';
+       String name="hehe";
+
+System.out.println("my num is:"+x);
+System.out.println("float:"+y);
+System.out.println("double:"+z);
+System.out.println(j);
+System.out.println(symbol);
+System.out.println(name);
+
+		}
+
+}
